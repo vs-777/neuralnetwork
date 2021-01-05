eab5d9022c6cc0f9834f90c75a1f15b77ba49a74
waskor <48648846+waskor@users.noreply.github.com>
2021-01-05 21:52:16 +0300

    Update ann.py

--- ann.py
+++ ann.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 class Network:
+    
     def __init__(self, shape, imgs, labels, lr):
-        
+        '''
+        Neural network class initialised with:
+        - list containing the structure for the hidden and output layers
+        - training images data
+        - training labels data
+        - learning rate
+        '''
         self.shape = shape
         self.imgs = imgs
         self.labels = labels
@@ -14,28 +21,27 @@ class Network:
         self.weights = [np.random.uniform(-0.5, 0.5, size=(layer,self.shape[i])) for i, layer in enumerate(self.shape[1:])] 
 
     def feedforward(self, i):
-        
+        '''
+        Calculate network layers with ReLU activation for hidden layers and softmax activation for output.
+        '''
         self.sample = i
         self.inputlayer = self.imgs[self.sample].flatten().reshape(-1,1)
         self.layers[0] = self.inputlayer
 
         for i in range(len(self.layers[1:-1])):
-            #hidden layer
             self.layers[i+1] = np.dot(self.weights[i], self.layers[i]) + self.biases[i]
-            #hidden layer relu
             self.layers[i+1] = np.maximum(0,self.layers[i+1])
 
-        #output layer
         self.layers[-1] = np.dot(self.weights[-1], self.layers[-2]) + self.biases[-1]
-        #output layer softmax
         self.layers[-1] = np.exp(self.layers[-1]-np.max(self.layers[-1]))/(np.exp(self.layers[-1]-np.max(self.layers[-1])).sum(axis=0))
 
     def backprop(self):
-        
+        '''
+        Back propopgation with stochastic gradient descent. Cross entropy loss function. 
+        '''
         true = np.zeros((10,1))
         true[self.labels[self.sample]]=1
 
-        #cross-entropy loss 
         loss = -np.sum(true*np.log2(self.layers[-1]))
 
         self.dcdw = []
@@ -52,13 +58,17 @@ class Network:
             dadz[dadz>0] = 1
 
     def update(self):
-        
+        '''
+        Adjust weights and biases based on calculated partial derivatives.
+        '''
         for i in range(len(self.layers[1:])):
             self.weights[i] -= self.lr*np.transpose(self.dcdw[i])
             self.biases[i] -= self.lr*self.dcdb[i]
 
     def train(self, epoch):
-        
+        '''
+        Train network.
+        '''
         for e in range(epoch):
             for i in range(self.imgs.shape[0]):
 
@@ -74,7 +84,9 @@ class Network:
         print("training complete")
 
     def test(self, imgs_test, labels_test, weights = None, biases = None):
-        
+        '''
+        Evaluate test data.  Weights and biases from previous runs can be given as arguments.
+        '''
         if weights is not None and biases is not None:
             self.weights = weights
             self.biases = biases
